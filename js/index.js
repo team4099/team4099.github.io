57295759577c41ef31797bcc87fabfbea70bb756
Parth Oza <plusparth@gmail.com>
2018-05-10 11:10:44 -0400

    Add JS to make high res bg image load

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 $("#highres-robot").on('load', function() {
     $("#site-wrapper").css('background-image', 'linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url(' + $("#highres-robot").prop('src') + ')');
-});
+}).each(function(){
+    if(this.complete) {
+      $(this).trigger('load');
+    }
+  });
